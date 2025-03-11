e441b096d190c4e4f96e8c252c6a7f29fa47a412
Christopher Schinnerl <cschinnerl@MacBook-Pro-von-Christopher.local>
2025-03-11 12:02:48 +0100

    copy walletd

---
+++ api/api.go
@@ -0,0 +1,263 @@
+package api
+
+import (
+	"encoding/json"
+	"time"
+
+	"go.sia.tech/core/consensus"
+	"go.sia.tech/core/types"
+	"go.sia.tech/walletd/wallet"
+)
+
+// A StateResponse returns information about the current state of the walletd
+// daemon.
+type StateResponse struct {
+	Version   string           `json:"version"`
+	Commit    string           `json:"commit"`
+	OS        string           `json:"os"`
+	BuildTime time.Time        `json:"buildTime"`
+	StartTime time.Time        `json:"startTime"`
+	IndexMode wallet.IndexMode `json:"indexMode"`
+}
+
+// A GatewayPeer is a currently-connected peer.
+type GatewayPeer struct {
+	Address string `json:"address"`
+	Inbound bool   `json:"inbound"`
+	Version string `json:"version"`
+
+	FirstSeen      time.Time     `json:"firstSeen,omitempty"`
+	ConnectedSince time.Time     `json:"connectedSince,omitempty"`
+	SyncedBlocks   uint64        `json:"syncedBlocks,omitempty"`
+	SyncDuration   time.Duration `json:"syncDuration,omitempty"`
+}
+
+// TxpoolBroadcastRequest is the request type for /txpool/broadcast.
+type TxpoolBroadcastRequest struct {
+	Basis          types.ChainIndex      `json:"basis"`
+	Transactions   []types.Transaction   `json:"transactions"`
+	V2Transactions []types.V2Transaction `json:"v2transactions"`
+}
+
+// TxpoolTransactionsResponse is the response type for /txpool/transactions.
+type TxpoolTransactionsResponse struct {
+	Basis          types.ChainIndex      `json:"basis"`
+	Transactions   []types.Transaction   `json:"transactions"`
+	V2Transactions []types.V2Transaction `json:"v2transactions"`
+}
+
+// TxpoolUpdateV2TransactionsRequest is the request type for /txpool/transactions/v2/basis.
+type TxpoolUpdateV2TransactionsRequest struct {
+	Basis        types.ChainIndex      `json:"basis"`
+	Target       types.ChainIndex      `json:"target"`
+	Transactions []types.V2Transaction `json:"transactions"`
+}
+
+// TxpoolUpdateV2TransactionsResponse is the response type for /txpool/transactions/v2/basis.
+type TxpoolUpdateV2TransactionsResponse struct {
+	Basis        types.ChainIndex      `json:"basis"`
+	Transactions []types.V2Transaction `json:"transactions"`
+}
+
+// BalanceResponse is the response type for /wallets/:id/balance.
+type BalanceResponse wallet.Balance
+
+// WalletReserveRequest is the request type for /wallets/:id/reserve.
+type WalletReserveRequest struct {
+	SiacoinOutputs []types.SiacoinOutputID `json:"siacoinOutputs"`
+	SiafundOutputs []types.SiafundOutputID `json:"siafundOutputs"`
+}
+
+// A WalletUpdateRequest is a request to update a wallet
+type WalletUpdateRequest struct {
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Metadata    json.RawMessage `json:"metadata"`
+}
+
+// WalletReleaseRequest is the request type for /wallets/:id/release.
+type WalletReleaseRequest struct {
+	SiacoinOutputs []types.SiacoinOutputID `json:"siacoinOutputs"`
+	SiafundOutputs []types.SiafundOutputID `json:"siafundOutputs"`
+}
+
+// WalletFundRequest is the request type for /wallets/:id/fund.
+type WalletFundRequest struct {
+	Transaction   types.Transaction `json:"transaction"`
+	Amount        types.Currency    `json:"amount"`
+	ChangeAddress types.Address     `json:"changeAddress"`
+}
+
+// WalletFundSFRequest is the request type for /wallets/:id/fundsf.
+type WalletFundSFRequest struct {
+	Transaction   types.Transaction `json:"transaction"`
+	Amount        uint64            `json:"amount"`
+	ChangeAddress types.Address     `json:"changeAddress"`
+	ClaimAddress  types.Address     `json:"claimAddress"`
+}
+
+// WalletFundResponse is the response type for /wallets/:id/fund.
+type WalletFundResponse struct {
+	Basis       types.ChainIndex    `json:"basis"`
+	Transaction types.Transaction   `json:"transaction"`
+	ToSign      []types.Hash256     `json:"toSign"`
+	DependsOn   []types.Transaction `json:"dependsOn"`
+}
+
+// WalletConstructRequest is the request type for /wallets/:id/construct.
+type WalletConstructRequest struct {
+	Siacoins      []types.SiacoinOutput `json:"siacoins"`
+	Siafunds      []types.SiafundOutput `json:"siafunds"`
+	ChangeAddress types.Address         `json:"changeAddress"`
+}
+
+// SignaturePayload is a signature that is required to finalize a transaction.
+type SignaturePayload struct {
+	PublicKey types.PublicKey `json:"publicKey"`
+	SigHash   types.Hash256   `json:"sigHash"`
+}
+
+// WalletConstructResponse is the response type for /wallets/:id/construct/transaction.
+type WalletConstructResponse struct {
+	Basis        types.ChainIndex    `json:"basis"`
+	ID           types.TransactionID `json:"id"`
+	Transaction  types.Transaction   `json:"transaction"`
+	EstimatedFee types.Currency      `json:"estimatedFee"`
+}
+
+// WalletConstructV2Response is the response type for /wallets/:id/construct/v2/transaction.
+type WalletConstructV2Response struct {
+	Basis        types.ChainIndex    `json:"basis"`
+	ID           types.TransactionID `json:"id"`
+	Transaction  types.V2Transaction `json:"transaction"`
+	EstimatedFee types.Currency      `json:"estimatedFee"`
+}
+
+// SeedSignRequest requests that a transaction be signed using the keys derived
+// from the given indices.
+type SeedSignRequest struct {
+	Transaction types.Transaction `json:"transaction"`
+	Keys        []uint64          `json:"keys"`
+}
+
+// RescanResponse contains information about the state of a chain rescan.
+type RescanResponse struct {
+	StartIndex types.ChainIndex `json:"startIndex"`
+	Index      types.ChainIndex `json:"index"`
+	StartTime  time.Time        `json:"startTime"`
+	Error      *string          `json:"error,omitempty"`
+}
+
+// An ApplyUpdate is a consensus update that was applied to the best chain.
+type ApplyUpdate struct {
+	Update consensus.ApplyUpdate `json:"update"`
+	State  consensus.State       `json:"state"`
+	Block  types.Block           `json:"block"`
+}
+
+// A RevertUpdate is a consensus update that was reverted from the best chain.
+type RevertUpdate struct {
+	Update consensus.RevertUpdate `json:"update"`
+	State  consensus.State        `json:"state"`
+	Block  types.Block            `json:"block"`
+}
+
+// ConsensusUpdatesResponse is the response type for /consensus/updates/:index.
+type ConsensusUpdatesResponse struct {
+	Applied  []ApplyUpdate  `json:"applied"`
+	Reverted []RevertUpdate `json:"reverted"`
+}
+
+// DebugMineRequest is the request type for /debug/mine.
+type DebugMineRequest struct {
+	Blocks  int           `json:"blocks"`
+	Address types.Address `json:"address"`
+}
+
+// SiacoinElementsResponse is the response type for any endpoint that returns
+// siacoin UTXOs
+type SiacoinElementsResponse struct {
+	Basis   types.ChainIndex       `json:"basis"`
+	Outputs []types.SiacoinElement `json:"outputs"`
+}
+
+// SiafundElementsResponse is the response type for any endpoint that returns
+// siafund UTXOs
+type SiafundElementsResponse struct {
+	Basis   types.ChainIndex       `json:"basis"`
+	Outputs []types.SiafundElement `json:"outputs"`
+}
+
+// ElementSpentResponse is the response type for /outputs/siacoin/:id/spent and
+// /outputs/siafund/:id/spent.
+type ElementSpentResponse struct {
+	Spent bool          `json:"spent"`
+	Event *wallet.Event `json:"event,omitempty"`
+}
+
+// MiningGetBlockTemplateRequest is the request type for
+// /mining/getblocktemplate.
+type MiningGetBlockTemplateRequest struct {
+	PayoutAddress types.Address `json:"payoutAddress,omitempty"`
+	LongPollID    string        `json:"longpollid,omitempty"`
+}
+
+// MiningGetBlockTemplateResponse is the response type for
+// /mining/getblocktemplate.
+type MiningGetBlockTemplateResponse struct {
+	Transactions      []MiningGetBlockTemplateResponseTxn `json:"transactions"`
+	MinerPayout       []MiningGetBlockTemplateResponseTxn `json:"minerpayout"`
+	PreviousBlockHash string                              `json:"previousblockhash"`
+
+	// Optional long polling from BIP 0022.
+	LongPollID string `json:"longpollid"`
+
+	// Basic pool extension from BIP 0023.
+	Target string `json:"target"`
+	Height uint32 `json:"height"`
+
+	// Mutations from BIP 0023.
+	Timestamp int32 `json:"curtime"`
+
+	// Block proposal from BIP 0023.
+	Version uint32 `json:"version"`
+	Bits    string `json:"bits"`
+}
+
+// MiningGetBlockTemplateResponseTxn is a transaction in a block template.
+type MiningGetBlockTemplateResponseTxn struct {
+	Data    string  `json:"data"`
+	Hash    string  `json:"hash"`
+	TxID    string  `json:"txid"`
+	Depends []int64 `json:"depends"`
+	Fee     int64   `json:"fee"`
+	SigOps  int64   `json:"sigops"`
+	TxType  string  `json:"txtype"`
+}
+
+// MiningSubmitBlockRequest is the request type for /mining/submitblock.
+type MiningSubmitBlockRequest struct {
+	// should contain only the hex-encoded block
+	Params []string `json:"params"`
+}
+
+// An AddSigningKeyRequest is a request to add an ed25519 signing key to the
+// key store.
+type AddSigningKeyRequest struct {
+	PrivateKey types.PrivateKey `json:"privateKey"`
+}
+
+// An AddSigningKeyResponse is the response to an AddSigningKeyRequest.
+type AddSigningKeyResponse struct {
+	PublicKey types.PublicKey `json:"publicKey"`
+}
+
+// A SignHashRequest is a request to sign a hash with a key.
+type SignHashRequest struct {
+	Hash types.Hash256 `json:"hash"`
+}
+
+// A SignHashResponse is the response to a SignHashRequest.
+type SignHashResponse struct {
+	Signature types.Signature `json:"signature"`
+}
---
+++ go.mod
@@ -0,0 +1,42 @@
+module go.sia.tech/walletd // v2.0.0
+
+go 1.23.1
+
+toolchain go1.23.2
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.24
+	go.sia.tech/core v0.10.3
+	go.sia.tech/coreutils v0.12.0
+	go.sia.tech/jape v0.12.1
+	go.sia.tech/web/walletd v0.29.0
+	go.uber.org/zap v1.27.0
+	golang.org/x/crypto v0.34.0
+	golang.org/x/term v0.29.0
+	gopkg.in/yaml.v3 v3.0.1
+	lukechampine.com/flagg v1.1.1
+	lukechampine.com/frand v1.5.1
+	lukechampine.com/upnp v0.3.0
+)
+
+require (
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/pprof v0.0.0-20230821062121-407c9e7a662f // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/onsi/ginkgo/v2 v2.12.0 // indirect
+	github.com/quic-go/qpack v0.5.1 // indirect
+	github.com/quic-go/quic-go v0.50.0 // indirect
+	github.com/quic-go/webtransport-go v0.8.1-0.20241018022711-4ac2c9250e66 // indirect
+	go.etcd.io/bbolt v1.4.0 // indirect
+	go.sia.tech/mux v1.4.0 // indirect
+	go.sia.tech/web v0.0.0-20240610131903-5611d44a533e // indirect
+	go.uber.org/mock v0.5.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+)
